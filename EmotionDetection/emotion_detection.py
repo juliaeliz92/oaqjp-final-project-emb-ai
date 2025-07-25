3cfec964bd2e5b41e21a7bb90be200723905684c
juliaeliz92 <juliapaulmyla@yahoo.com>
2025-07-25 01:34:53 -0400

    added testing class, server

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,12 +7,24 @@ def emotion_detector(text_to_analyse):  # Define a function named sentiment_anal
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers = header)  # Send a POST request to the API with the text and headers
     formatted_response = json.loads(response.text)
-    anger = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
-    disgust = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
-    fear = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
-    joy = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
-    sadness = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
-    anger = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
-    emotions = {"anger": anger, "disgust":disgust, "fear": fear, "joy": joy, "sadness": sadness}
-    dominant_emotion = max(emotions, key = emotions.get)
+
+    if response.status_code == 200:
+        anger = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
+        disgust = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
+        fear = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
+        joy = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
+        sadness = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
+        anger = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
+        emotions = {"anger": anger, "disgust":disgust, "fear": fear, "joy": joy, "sadness": sadness}
+        dominant_emotion = max(emotions, key = emotions.get)
+
+    elif response.status_code == 400:
+        anger = None
+        disgust = None
+        fear = None
+        joy = None
+        sadness = None
+        anger = None
+        dominant_emotion = None
+        
     return {"anger": anger, "disgust":disgust, "fear": fear, "joy": joy, "sadness": sadness, 'dominant_emotion': dominant_emotion}
